8d132929c733e14768a9e2a2a9a1284e8c14b87d
MDeiml <matthias@deiml.net>
2020-05-31 12:29:04 +0200

    Implement raytracing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 extern crate glium;
 extern crate rand;
 use glium::Surface;
-use rand::Rng;
 
 const FPS: u64 = 60;
 const DELTA: f32 = 1.0 / 60 as f32;
@@ -19,24 +18,85 @@ const VERTEX_SHADER_SRC: &str = r#"
 #version 140
 
 in vec3 position;
-uniform vec3 cam_position;
+out vec3 world_position;
 
 void main() {
-    mat4 matrix;
-    matrix[0] = vec4(1.0, 0.0, 0.0, 0.0);
-    matrix[1] = vec4(0.0, 1.0, 0.0, 0.0);
-    matrix[2] = vec4(0.0, 0.0, 1000.1/999.9, 1.0);
-    matrix[3] = vec4(0.0, 0.0, -200.0/999.9, 0.0);
-    gl_Position = matrix * vec4(position - cam_position, 1.0);
+    world_position = position;
+    gl_Position = vec4(position, 1.0);
 }
 "#;
 
 const RAYTRACING_SHADER_SRC: &str = r#"
-#version 140
+#version 430
 
 out vec4 color;
+in vec3 world_position;
+
+const vec3 SVO_SIZE = vec3(1.0, 1.0, 1.0);
+const vec3 SVO_POSITION = vec3(-0.75, -0.75, 1.0);
+const uint MAX_ITERATIONS = 100;
+const uint MAX_DEPTH = 1;
+const float EPS = 0.0001;
+
+uniform vec3 cam_position;
+
+layout(std430, binding = 0) buffer octree_buffer {
+    uint octree[];
+};
 
 void main() {
+    vec3 dir = normalize(world_position - cam_position);
+    vec3 dt = 1.0 / dir;
+    float t = 0.0;
+    vec3 t1 = (SVO_POSITION - cam_position) * dt;
+    vec3 t2 = ((SVO_POSITION + SVO_SIZE) - cam_position) * dt;
+    vec3 t_min0 = min(t1, t2);
+    vec3 t_max0 = max(t1, t2);
+    float t_min = max(t_min0.x, max(t_min0.y, t_min0.z));
+    float t_max = min(t_max0.x, min(t_max0.y, t_max0.z));
+    if (t_max < t_min || t_max < 0.0) {
+        color = vec4(0.0, 0.0, 0.0, 1.0);
+        return;
+    }
+    t = max(0.0, t_min);
+
+    for (uint i = 0; i < MAX_ITERATIONS; i++) {
+        if (t >= t_max) {
+            color = vec4(0.0, 0.0, 0.0, 0.0);
+            return;
+        }
+        vec3 pos = t * dir + cam_position;
+        vec3 cell_min = SVO_POSITION;
+        vec3 cell_max = SVO_POSITION + SVO_SIZE;
+        uint index = 0;
+        for (uint j = 0; j < MAX_DEPTH; j++) {
+            vec3 cell_mid = (cell_min + cell_max) / 2;
+            uvec3 child = uvec3(step(cell_mid, pos));
+            uint child_index = child.x + 2 * child.y + 4 * child.z;
+            cell_min = child * cell_mid + (1 - child) * cell_min;
+            cell_max = child * cell_max + (1 - child) * cell_mid;
+            uint child_mask = octree[index];
+            if (((child_mask >> child_index) & 1u) == 0) {
+                index = 0;
+                break;
+            } else {
+                index = octree[index + bitCount(child_mask << (31 - child_index))];
+            }
+        }
+        if (index != 0) {
+            uint data = octree[index];
+            color = vec4(
+                bitfieldExtract(data, 16, 8) / 255.0,
+                bitfieldExtract(data, 8, 8) / 255.0,
+                bitfieldExtract(data, 0, 8) / 255.0,
+                1.0
+            );
+            return;
+        }
+        t_max0 = max((cell_max - cam_position) * dt, (cell_min - cam_position) * dt);
+        t = min(t_max0.x, min(t_max0.y, t_max0.z)) + EPS;
+    }
+
     color = vec4(1.0, 0.0, 0.0, 1.0);
 }
 "#;
@@ -49,77 +109,42 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(1241);
-    let mut data = [[[([0u8, 0u8, 0u8], false, false); 16]; 32]; 16];
-    let mut vertices: Vec<Vertex> = Vec::new();
-
-    for z in 0..16 {
-        for y in 0..32 {
-            for x in 0..16 {
-                data[x][y][z] = (
-                    [
-                        (8 * x + rng.gen_range(0, 8)) as u8,
-                        (4 * y + rng.gen_range(0, 4)) as u8,
-                        (8 * z + rng.gen_range(0, 8)) as u8,
-                    ],
-                    rng.gen_bool(0.5),
-                    rng.gen_bool(0.5),
-                );
-            }
-        }
-    }
-
-    for _ in 0..2 {
-        for z in 0..16 {
-            for y in 0..32 {
-                for x in 0..16 {
-                    let (_, ax, az) = data[x][y][z];
-                    if ax && z < 15 {
-                        data[x][y][z].0[0] = data[x][y][z + 1].0[0];
-                    }
-                    if az && x < 15 {
-                        data[x][y][z].0[2] = data[x + 1][y][z].0[2];
-                    }
-                }
-            }
-        }
-    }
-    for z in 0..16 {
-        for y in 0..32 {
-            for x in 0..16 {
-                let ([px, py, pz], _, _) = data[x][y][z];
-                if z < 15 {
-                    let ([qx, qy, qz], _, _) = data[x][y][z + 1];
-                    if px == qx {
-                        vertices.push(Vertex {
-                            position: [px as f32, py as f32, pz as f32],
-                        });
-                        vertices.push(Vertex {
-                            position: [qx as f32, qy as f32, qz as f32],
-                        });
-                    }
-                }
-                if x < 15 {
-                    let ([qx, qy, qz], _, _) = data[x + 1][y][z];
-                    if pz == qz {
-                        vertices.push(Vertex {
-                            position: [px as f32, py as f32, pz as f32],
-                        });
-                        vertices.push(Vertex {
-                            position: [qx as f32, qy as f32, qz as f32],
-                        });
-                    }
-                }
-            }
-        }
-    }
+    let vertices = vec![
+        Vertex {
+            position: [-1.0, -1.0, 1.0],
+        },
+        Vertex {
+            position: [-1.0, 1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, -1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, 1.0, 1.0],
+        },
+    ];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &vertices).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
     let program =
         glium::Program::from_source(&display, VERTEX_SHADER_SRC, RAYTRACING_SHADER_SRC, None)
             .unwrap();
-    let mut cam_position: [f32; 3] = [64.0, 64.0, -10.0];
+
+    let mut octree_buffer = glium::buffer::Buffer::<[u32]>::empty_unsized(
+        &display,
+        glium::buffer::BufferType::ShaderStorageBuffer,
+        5 * 4,
+        glium::buffer::BufferMode::Persistent,
+    )
+    .unwrap();
+
+    octree_buffer.map()[0] = 0b00010001;
+    octree_buffer.map()[1] = 3;
+    octree_buffer.map()[2] = 4;
+    octree_buffer.map()[3] = 0xffff00;
+    octree_buffer.map()[4] = 0x00ff00;
+
+    let mut cam_position: [f32; 3] = [0.0, 0.0, 0.0];
     let mut cam_speed: [f32; 3] = [0.0, 0.0, 0.0];
 
     event_loop.run(move |ev, _, control_flow| {
@@ -196,7 +221,8 @@ fn main() {
                 &indices,
                 &program,
                 &uniform! {
-                    cam_position: cam_position
+                    cam_position: cam_position,
+                    octree_buffer: &octree_buffer
                 },
                 &Default::default(),
             )
